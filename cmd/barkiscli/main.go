48f415b6c068fe5e8b7bed5ed48832377c937fdb
barkisnet <57025073+barkisnet@users.noreply.github.com>
2021-04-16 16:52:33 +0800

    Update main.go

--- cmd/barkiscli/main.go
+++ cmd/barkiscli/main.go
@@ -40,9 +40,8 @@ func main() {
 	config.SetBech32PrefixForConsensusNode(sdk.Bech32PrefixConsAddr, sdk.Bech32PrefixConsPub)
 	config.Seal()
 
-	// TODO: setup keybase, viper object, etc. to be passed into
-	// the below functions and eliminate global vars, like we do
-	// with the cdc
+	// TODO: setup keybase, viper object, etc. to be passed into the below
+	// functions and eliminate global vars, like we do with the cdc
 
 	rootCmd := &cobra.Command{
 		Use:   "barkiscli",
