91c8065813afe430ad116ddf7d34ddbd458d010d
barkisnet <57025073+barkisnet@users.noreply.github.com>
2021-05-12 15:10:49 +0800

    Update codec.go

--- x/genaccounts/internal/types/codec.go
+++ x/genaccounts/internal/types/codec.go
@@ -7,7 +7,7 @@ import (
 // ModuleName is "accounts"
 const ModuleName = "accounts"
 
-// ModuleCdc - generic sealed codec to be used throughout this module
+// ModuleCdc generic sealed codec to be used throughout this module
 var ModuleCdc *codec.Codec
 
 func init() {
